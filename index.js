e4f122a929d82a1d4bd7621313bf3f34f8662d3a
ccdawson2 <chrisdwson@gmail.com>
2017-05-11 21:26:15 +1000

    vue Event Demo

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+app.use(express.static(__dirname + ''));
+
+app.get('*', function(req, res) {
+	var options = { root: __dirname };
+    res.sendFile('index.html',options);
+});
+
+var port = "3000";
+app.listen(port);
+console.log("Listening to Port: " + port);
